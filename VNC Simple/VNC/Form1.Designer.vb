735eee8f9bf3f68d4509b3747480c3ddc1521a08
eam2001 <48334007+eam2001@users.noreply.github.com>
2019-05-14 11:07:47 +0100

    V 1.0

---
+++ VNC Simple/VNC/Form1.Designer.vb
@@ -0,0 +1,146 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend
+        Me.ComboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(12, 12)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(351, 21)
+        Me.ComboBox1.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(369, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(95, 21)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Abrir"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(12, 39)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(103, 17)
+        Me.CheckBox1.TabIndex = 33
+        Me.CheckBox1.Text = "VNC Auto Scale"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 62)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(136, 173)
+        Me.ListBox1.TabIndex = 34
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(154, 62)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(95, 21)
+        Me.Button2.TabIndex = 35
+        Me.Button2.Text = "ping"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 500
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(154, 89)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(95, 21)
+        Me.Button3.TabIndex = 36
+        Me.Button3.Text = "Editar config"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(151, 199)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(115, 13)
+        Me.Label1.TabIndex = 37
+        Me.Label1.Text = "Open Source Software"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(151, 222)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(257, 13)
+        Me.Label2.TabIndex = 38
+        Me.Label2.Text = "https://github.com/eam2001/Simple-VNC-Connector"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(479, 253)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximumSize = New System.Drawing.Size(495, 292)
+        Me.MinimumSize = New System.Drawing.Size(495, 292)
+        Me.Name = "Form1"
+        Me.Text = "VNC 1.0"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ VNC Simple/VNC/Form1.vb
@@ -0,0 +1,97 @@
+﻿Imports System.IO
+Public Class Form1
+
+    Public Links
+    Public ComboNames
+    Public VNCpath
+    Public AutoScaling = ""
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        Dim reader As StreamReader = My.Computer.FileSystem.OpenTextFileReader(CurDir() & "\VNC.CSV")
+        Dim linha = reader.ReadLine
+        linha = reader.ReadLine
+        Dim n = 0
+        Do While linha <> "Configuration"
+            ReDim Preserve Links(n)
+            ReDim Preserve ComboNames(n)
+            Links(n) = linha
+            Dim temp = Split(linha, ";")
+            ComboNames(n) = temp(1) & " - " & temp(2) & " - " & temp(3)
+            n = n + 1
+            linha = reader.ReadLine
+        Loop
+       
+        VNCpath = reader.ReadLine
+
+        reader.Close()
+
+
+        ComboBox1.DataSource = ComboNames
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Try
+            Dim temp1 = Split(Links(ComboBox1.SelectedIndex), ";")
+            Process.Start(VNCpath & " ", Trim(temp1(3)) & AutoScaling & " -password " & temp1(5))
+        Catch ex As Exception
+            MsgBox("problemas ao lançar o VNC")
+        End Try
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked = True Then
+            AutoScaling = " -autoscaling"
+        Else
+            AutoScaling = ""
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Dim temp2 = Split(Links(ComboBox1.SelectedIndex), ";")
+
+
+        If My.Computer.Network.Ping(Trim(temp2(3))) Then
+            ListBox1.Items.Insert(0, "success - " & Trim(temp2(3)))
+        Else
+            ListBox1.Items.Insert(0, "no reply - " & Trim(temp2(3)))
+        End If
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        ListBox1.Items.Clear()
+        If Timer1.Enabled = False Then
+            Timer1.Enabled = True
+            Button2.Text = "Stop"
+        Else
+            Timer1.Enabled = False
+            Button2.Text = "Start"
+        End If
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComboBox1.SelectedIndexChanged
+        ListBox1.Items.Clear()
+        Dim temp3 = Split(Links(ComboBox1.SelectedIndex), ";")
+        ListBox1.Items.Add(Trim(temp3(0)))
+        ListBox1.Items.Add(Trim(temp3(1)))
+        ListBox1.Items.Add(Trim(temp3(2)))
+        ListBox1.Items.Add(Trim(temp3(3)))
+        ListBox1.Items.Add(Trim(temp3(4)))
+        ListBox1.Items.Add(Trim(temp3(5)))
+
+    End Sub
+
+    Private Sub ComboBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles ComboBox1.KeyPress
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Shell("notepad.exe " & CurDir() & "\VNC.CSV", AppWinStyle.MaximizedFocus)
+    End Sub
+
+    Private Sub ComboBox1_KeyDown(sender As Object, e As KeyEventArgs) Handles ComboBox1.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            Button1_Click(sender, e)
+        End If
+    End Sub
+End Class
